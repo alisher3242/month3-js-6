d2a7e24beb869b0fb51ec9c60c80845816301ca2
alisher <alisher.tojimov@yandex.ru>
2022-04-05 13:25:02 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// 1-misol
+
+// let n = prompt("Raqam kiriting:");
+
+// for(let i=2; i<=10; i++ ){
+//     console.log(i*n);
+// };
+
+//2-misol
+
+let num1 = prompt("Birinchi sonni kiriting:");
+num1 = Number(num1)
+let operator = prompt("Amalni kiritng:");
+let num2 = prompt("Ikkinchi sonni kiriting:");
+num2 = Number(num2)
+
+if (operator == "+" ) {
+    console.log("Natijangiz: " + (num1+num2) );
+}else if (operator == "-") {
+    console.log("Natijangiz: " + (num1-num2));
+}else if(operator == "*") {
+    console.log("Natijangiz: " + (num1*num2));
+}else if(operator == "/") {
+    console.log("Natijangiz: " + (num1/num2));
+}else {
+    console.log("Tog'ri amal kiriting: ");
+}
